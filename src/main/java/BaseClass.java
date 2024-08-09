f3275c7308e5c5a419d4d4c3e824e01fa891492d
anushka <anushkamurgudkar@gmail.com>
2024-08-09 10:48:24 +0530

    2nd changes

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 
@@ -23,11 +24,11 @@ public class BaseClass {
 //	public DatabaseUtility dlib = new DatabaseUtility();
 //	public static WebDriver sdriver = null;
 //
-//	@BeforeSuite(alwaysRun = true)
-//	public void configBS() throws Throwable {
-//		dlib.getDbConnection();
-//
-//	}
+	@BeforeSuite(alwaysRun = true)
+	public void configBS() throws Throwable {
+		//dlib.getDbConnection();
+
+	}
 
 //	@BeforeClass(groups = "ST")
 //	public void configBs() throws Throwable {
